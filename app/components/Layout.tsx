d5ddd05cd74fd711d53c21765ad6fd9c1d0e6b5b
Zoynul <33788041+zoynulabedin@users.noreply.github.com>
2025-08-22 21:42:47 +0600

    Update Layout.tsx update logo url

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -67,7 +67,7 @@ export default function Layout({ children }: LayoutProps) {
           <div className="flex items-center justify-between md:grid md:grid-cols-3 md:gap-4">
             {/* Logo */}
             <div className="flex items-center">
-              <Link to="/" className="text-3xl font-bold text-red-600">
+              <Link to="https://www.snowlightentertainment.com/" className="text-3xl font-bold text-red-600">
                 Snowlight!
               </Link>
             </div>
